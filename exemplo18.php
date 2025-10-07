60bfec3c58a296ab9774100c3c6d7644500277cc
Albinone <jose.rei.albino@gmail.com>
2025-10-07 23:16:24 +0100

    Aulas

--- exemplo18.php
+++ exemplo18.php
@@ -3,10 +3,28 @@
 //XML
 
 //read XML
-    //DOMDocument
-    //SimpleXMLElement
+    //DOMDocument (big files)    //SimpleXMLElement (small files )
 
+$xml = '<?xml version="1.0" encoding="UTF-8"?>
+<accounts>
+    <account type="X">https://www.x.com</account>
+    <account type="facebook">https://www.facebook.com</account>
+    <account type="instagram">https://www.instagram.com</account>
+    <account type="linkedin">https://www.linkedin.com</account>
+    <account type="github" id="2">https://www.github.com</account>
+</accounts>';
 
+//DOMDocument
+$dom = new DOMDocument();
+$dom->loadXML($xml);
+$accounts = $dom->getElementsByTagName(qualifiedName:'account');
+
+foreach($accounts as $account){
+    $name = $account->getAttribute('type');
+    $id = $account->getAttribute('id');
+    $url = $account->nodeValue;
+    echo $name . " : " . $url . "<br>". "id " . $id;
+}                                   
 
 
 
