01df438a9ea6f520878c770207d4b882bd109e78
Albinone <jose.rei.albino@gmail.com>
2025-10-08 19:22:18 +0100

    Aulas

--- exemplo18.php
+++ exemplo18.php
@@ -17,13 +17,32 @@ $xml = '<?xml version="1.0" encoding="UTF-8"?>
 //DOMDocument
 $dom = new DOMDocument();
 $dom->loadXML($xml);
-$accounts = $dom->getElementsByTagName(qualifiedName:'account');
-
+$accounts = $dom->getElementsByTagName(qualifiedName:"account");
 foreach($accounts as $account){
     $name = $account->getAttribute('type');
     $id = $account->getAttribute('id');
     $url = $account->nodeValue;
     echo $name . " : " . $url . "<br>". "id " . $id;
 }                                   
+
+//write XML
+//SimpleXMLElement
+
+echo "<hr>";
+
+$accounts2 = new SimpleXMLElement(data:'<?xml version="1.0" encoding="UTF-8"?><accounts></accounts>');
+$accounts2->addChild(qualifiedName:'account', value:'https://www.x.com')->addAttribute(qualifiedName:'type', value:'X');
+$accounts2->addChild(qualifiedName:'account', value:'https://www.facebook.com')->addAttribute(qualifiedName:'type', value:'facebook');
+$accounts2->addChild(qualifiedName:'account', value:'https://www.instagram.com')->addAttribute(qualifiedName:'type', value:'instagram');
+$accounts2->addChild(qualifiedName:'account', value:'https://www.linkedin.com')->addAttribute(qualifiedName:'type', value:'linkedin');
+$accounts2->addChild(qualifiedName:'account', value:'https://www.github.com')->addAttribute(qualifiedName:'type', value:'github');//->addAttribute(qualifiedName:'id', value:'2');
+
+$finalXML = $accounts2->asXML();
+echo $finalXML;
+
+echo "<hr>";
+
+
+
 
 ?>
