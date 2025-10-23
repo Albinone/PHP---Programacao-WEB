a9911acfaa28f6df1f82152c2383e38669aad65a
Albinone <jose.rei.albino@gmail.com>
2025-10-23 22:41:53 +0100

    Aulas

--- Integração sistemas/nullvsundefined.js
+++ Integração sistemas/nullvsundefined.js
@@ -13,3 +13,14 @@ let person = {name: "Person1"};
 
 console.log(person.name); // undefined
 console.log(person.age); // undefined
+
+let fruits = ["Apple", "Banana", "Orange"];
+
+console.log(fruits[0]); // undefined
+console.log(fruits[5]); // undefined
+
+var varNull = null;
+var initialized;
+
+console.log(varNull==initialized);  // null
+console.log(varNull===initialized); // false
